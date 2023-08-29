a7c2ab90b0cb66e4e9f7ad73afe2142e66274b9e
Matthew Maurier <matthewmaurier@gmail.com>
2023-08-28 20:21:54 -0600

    Dark mode switch

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
-const prompt = require('prompt-sync')();
+const dialContainer = document.querySelector('.dial-container');
+const currentTheme = localStorage.getItem('theme');
 
+if (currentTheme == 'dark') {
+    document.body.classList.add('dark-mode');
+}
+
+dialContainer.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    
+    let theme = 'light';
+    if (document.body.classList.contains('dark-mode')) {
+        theme = 'dark';
+    }
+    
+    localStorage.setItem('theme', theme);
+});
